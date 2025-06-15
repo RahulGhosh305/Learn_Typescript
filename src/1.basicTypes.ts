43fe36aff23e87acebd072405b877030fc48d643
Rahul Ghosh <rahul.info305@gmail.com>
2025-06-15 19:57:26 +0600

    primitive data type

--- src/1.basicTypes.ts
+++ src/1.basicTypes.ts
@@ -1,2 +1,30 @@
-let course: string = "Learn TypeScript"
-console.log(course)
+/**
+ **  Basic Data Types
+ **  Primitive Data Type: String, Number, Boolean, Null, Undefined, Symbol
+ **  Non Primitive Data Type: Object, Array, Tuple, Function
+ **/
+
+// Primetive Data Types
+// String
+const courseName: string = "Typescript";
+
+// Number
+const courseDuration: number = 6.5;
+
+// Boolean
+const isAdvance: boolean = true;
+
+// Null
+const noValue: null = null;
+
+// Undefined
+const x: undefined = undefined;
+
+// Non Primitive Data Types
+// Array
+const employees: string[] = ["Rahul", "Joy"];
+const employeesIds: number[] = [1, 2];
+
+//Tuple
+const cordinates: [number, number] = [12.233, 23.322];
+const info: [string, number, boolean] = ["Rahul", 28, true];
