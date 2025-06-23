ed560304b5d35b94ad2e801e8b5901178c9cdc3f
Rahul Ghosh <rahul.info305@gmail.com>
2025-06-23 08:44:46 +0600

    inheritance

--- dist/3.oopTypes.js
+++ dist/3.oopTypes.js
@@ -1,7 +1,7 @@
 "use strict";
 /**
  **  Class
- **
+ **  OOP -> Inheritance
  **
  **
  **
@@ -22,10 +22,9 @@ class Animan {
         console.log(`${this.name} is call by ${this.sound}`);
     }
 }
-const dog = new Animan("German Shepad", "Dog", "Ghew Ghew");
-console.log(dog.makeSound());
-// Alternative Above
+// Alternative above class
 class AnimalAlternative {
+    // Parameter Properties
     constructor(name, species, sound) {
         this.name = name;
         this.species = species;
@@ -35,5 +34,43 @@ class AnimalAlternative {
         console.log(`${this.name} is call by ${this.sound}`);
     }
 }
-const cat = new AnimalAlternative("Persian", "Cat", "Maew Maew");
-console.log(cat.makeSound());
+const dog = new AnimalAlternative("German Shepard", "Dog", "Ghew Ghew"); // Create new Dog Instance
+dog.makeSound();
+const cat = new AnimalAlternative("Persian", "Cat", "Maew Maew"); // Create new Cat Instance
+cat.makeSound();
+// OOP --> Inheritance
+// Parent Class
+class PersonClass {
+    constructor(name, age, address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+    getSleep(numOfHours) {
+        console.log(`${this.name} will sleep ${numOfHours}`);
+    }
+}
+// Child Class
+class StudentClass extends PersonClass {
+    constructor(name, age, address) {
+        super(name, age, address);
+    }
+}
+{
+    const student1 = new StudentClass("Mr. X", 20, "Dhaka");
+    student1.getSleep(10);
+}
+// Child Class
+class Teacher extends PersonClass {
+    constructor(name, age, address, designation) {
+        super(name, age, address);
+        this.designation = designation;
+    }
+    takeClasses(numOfClass) {
+        console.log(`${this.name} will take ${numOfClass}`);
+    }
+}
+{
+    const teacher = new Teacher("Mr. X", 20, "Dhaka", "Professor");
+    teacher.getSleep(8);
+}
--- src/3.oopTypes.ts
+++ src/3.oopTypes.ts
@@ -1,6 +1,6 @@
 /**
  **  Class
- **
+ **  OOP -> Inheritance
  **
  **
  **
@@ -46,3 +46,50 @@ dog.makeSound();
 
 const cat = new AnimalAlternative("Persian", "Cat", "Maew Maew"); // Create new Cat Instance
 cat.makeSound();
+
+// OOP --> Inheritance
+// Parent Class
+class PersonClass {
+  name: string;
+  age: number;
+  address: string;
+
+  constructor(name: string, age: number, address: string) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+
+  getSleep(numOfHours: number) {
+    console.log(`${this.name} will sleep ${numOfHours}`);
+  }
+}
+// Child Class
+class StudentClass extends PersonClass {
+  constructor(name: string, age: number, address: string) {
+    super(name, age, address);
+  }
+}
+
+{
+  const student1 = new StudentClass("Mr. X", 20, "Dhaka");
+  student1.getSleep(10);
+}
+// Child Class
+class Teacher extends PersonClass {
+  designation: string;
+
+  constructor(name: string, age: number, address: string, designation: string) {
+    super(name, age, address);
+    this.designation = designation;
+  }
+
+  takeClasses(numOfClass: number) {
+    console.log(`${this.name} will take ${numOfClass}`);
+  }
+}
+
+{
+  const teacher = new Teacher("Mr. X", 20, "Dhaka", "Professor");
+  teacher.getSleep(8);
+}
